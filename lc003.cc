54fdea432a45503fc8e116e51bf86bc3bf2c5f22
ChallenAi <jianglun465@sina.com>
2019-09-12 19:22:06 +0800

    finish lc3

--- lc003.cc
+++ lc003.cc
@@ -1,9 +1,34 @@
 #include <string>
+#include <iostream>
 using namespace std;
 
 class Solution {
 public:
     int lengthOfLongestSubstring(string s) {
-        return 3;
+        int max_len = 0;
+        int left = 0;
+        char current_char;
+        for (int i = 0; i < s.size(); i++)
+        {
+            // map str
+
+            // judge if new char  appears before
+            // if appears: move left+n, move right++
+
+            current_char = s.at(i);
+            bool need_update_max = true;
+            for (int idx = left; idx < i; idx++) {
+                if (s.at(idx) == current_char) {
+                    left = idx+1;
+                    need_update_max = false;
+                }
+            }
+
+            if (need_update_max && i - left +1 > max_len) {
+                max_len = i - left+1;
+            }
+        }
+        
+        return max_len;
     }
 };
--- lc003_test.cc
+++ lc003_test.cc
@@ -6,8 +6,9 @@ int main(int argc, char const *argv[])
     string str1 = "pwwkew";
     string str2 = "abcabcbb";
     string str3 = "bbbbbabbbbbbbb";
+    string str4 = "bbbbbbbbbbbbbb";
     Solution s;
-    if (s.lengthOfLongestSubstring(str1) == 3 && s.lengthOfLongestSubstring(str2) == 3 && s.lengthOfLongestSubstring(str3) == 2) {
+    if (s.lengthOfLongestSubstring(str1) == 3 && s.lengthOfLongestSubstring(str2) == 3 && s.lengthOfLongestSubstring(str3) == 2 && s.lengthOfLongestSubstring(str4) == 1) {
         std::cout << "pass" << std::endl;
     } else {
         std::cout << "not pass!" << std::endl;
