5b799c2b65eb98a268ad68c66ce0777ce8cb56aa
ChallenAi <jianglun465@sina.com>
2019-09-13 16:41:57 +0800

    realize lc005cc

--- lc005.cc
+++ lc005.cc
@@ -4,6 +4,43 @@ using namespace std;
 class Solution {
 public:
     string longestPalindrome(string s) {
+        int left, right;
+        string current_max = s.substr(0,1);
         
+        for (int i = 1; i < s.size(); i++) {
+            // 1. i is the mid
+            left = i-1;
+            right = i+1;
+            while (left >= 0 && right < s.size())
+            {
+                if (s.at(left) == s.at(right)) {
+                    if ((right - left + 1) > current_max.size()) {
+                        // substr(pos, offset_n)
+                        current_max = s.substr(left, right-left+1);
+                    }
+                    left--;
+                    right++;
+                } else {
+                    break;
+                }
+            }
+
+            // 2. i and i-1 is the mid
+            left = i-1;
+            right = i;
+            while (left >= 0 && right < s.size())
+            {
+                if (s.at(left) == s.at(right)) {
+                    if ((right - left + 1) > current_max.size()) {
+                        current_max = s.substr(left, right-left+1);
+                    }
+                    left--;
+                    right++;
+                } else {
+                    break;
+                }
+            }
+        }
+        return current_max;
     }
 };
--- lc005_test.cc
+++ lc005_test.cc
@@ -1,11 +1,15 @@
 #include <iostream>
+#include <string>
 #include "lc005.cc"
 
 int main(int argc, char const *argv[])
 {
+    string str1 = "babad";
+    string str2 = "cbbd";
     Solution s;
-    ListNode* l3 = s.addTwoNumbers(l1, l2);
-    if (l3->val == 7) {
+    string resu1 = s.longestPalindrome(str1);
+    string resu2 = s.longestPalindrome(str2);
+    if ((resu1 == "aba" || resu1 == "bab") && resu2 == "bb") {
         std::cout << "pass" << std::endl;
     } else {
         std::cout << "not pass!" << std::endl;
