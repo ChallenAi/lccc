4a85fea27d373d979373a673fb3513f8e0a6cd15
ChallenAi <jianglun465@sina.com>
2019-09-13 16:01:22 +0800

    lc004 not pass...

--- lc004.cc
+++ lc004.cc
@@ -4,10 +4,43 @@ using namespace std;
 class Solution {
 public:
     double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
-        
-        int ptr1, ptr2;
+        bool twoMid = (nums1.size() + nums2.size())%2 == 0;
+        int midIdx1 = (nums1.size() + nums2.size() - 1)/2;
+        double midVal;
+
+        int ptr1 = 0, ptr2 = 0;
+        int cnt = 0;
         while (ptr1 < nums1.size() || ptr2 < nums2.size())
         {
+            if (cnt == midIdx1) {
+                if (nums1[ptr1] < nums2[ptr2]) {
+                    midVal = nums1[ptr1];
+                    ptr1++;
+                } else {
+                    midVal = nums2[ptr2];
+                    ptr2++;
+                }
+
+                if (twoMid) {
+                    if (ptr1 >= nums1.size()) {
+                        midVal = (nums2[ptr2] + midVal) / 2;
+                    } else if (ptr2 < nums2.size()) {
+                        midVal = (nums1[ptr1] + midVal) / 2;
+                    } else if (nums1[ptr1] < nums2[ptr2]) {
+                        midVal = (nums1[ptr1] + midVal) / 2;
+                    } else {
+                        midVal = (nums2[ptr2] + midVal) / 2;
+                    }
+                }
+                
+                return midVal;
+            }
+            if (nums1[ptr1] < nums2[ptr2]) {
+                ptr1++;
+            } else {
+                ptr2++;
+            }
+            cnt++;
         }
         
         return 0.0f;
--- lc004_test.cc
+++ lc004_test.cc
@@ -7,21 +7,23 @@ int main(int argc, char const *argv[])
     int nums1_[] = {1, 3};
     int nums2_[] = {2};
     vector<int> nums1 = vector<int>(nums1_, nums1_+2);
-    vector<int> nums2 = vector<int>(nums2_, nums2_+2);
+    vector<int> nums2 = vector<int>(nums2_, nums2_+1);
 
-    int nums3_[] = {1, 2, 3};
-    int nums4_[] = {4, 5, 6};
+    int nums3_[] = {1, 2};
+    int nums4_[] = {3, 4};
     vector<int> nums3 = vector<int>(nums3_, nums3_+2);
     vector<int> nums4 = vector<int>(nums4_, nums4_+2);
 
     Solution s;
-    double resu1 = s.findMedianSortedArrays(nums1, nums2);
+    // double resu1 = s.findMedianSortedArrays(nums1, nums2);
     double resu2 = s.findMedianSortedArrays(nums3, nums4);
+
+    std::cout << resu2 << std::endl;
     
-    if (resu1 == 2.0f && resu2 == 3.5f) {
-        std::cout << "pass" << std::endl;
-    } else {
-        std::cout << "not pass!" << std::endl;
-    }
+    // if (resu1 == 2.0f && resu2 == 2.5f) {
+    //     std::cout << "pass" << std::endl;
+    // } else {
+    //     std::cout << "not pass!" << std::endl;
+    // }
     return 0;
 }
