b2a8cc20293d75c220d592fdfe75e560d9c693fa
ChallenAi <jianglun465@sina.com>
2019-09-17 12:27:55 +0800

    AC 023

--- lc023.cc
+++ lc023.cc
@@ -9,6 +9,30 @@ struct ListNode {
 class Solution {
 public:
     ListNode* mergeKLists(vector<ListNode*>& lists) {
-        
+        ListNode *resu = new ListNode(0);
+        ListNode *ptr = resu;
+        while (lists.size() > 0)
+        {
+            int min_idx = 0;
+            ListNode *min_node = lists[0];
+            for (int i = 0; i < lists.size(); ++i) {
+                if (lists[i]->val < min_node->val) {
+                    min_node = lists[i];
+                    min_idx = i;
+                }
+            }
+            ptr->next = new ListNode(min_node->val);
+            ptr = ptr->next;
+            if (lists[min_idx]->next == NULL) {
+                // delete current idx
+                vector<ListNode*>::iterator iter = lists.begin();
+                lists.erase(iter+min_idx);
+            } else {
+                lists[min_idx] = lists[min_idx]->next;
+            }
+        }
+
+        resu = resu->next;
+        return resu;
     }
 };
--- lc023_test.cc
+++ lc023_test.cc
@@ -1,14 +1,43 @@
 #include <iostream>
 #include <vector>
-#include "lc016.cc"
+#include "lc023.cc"
 
 int main(int argc, char const *argv[])
 {
-    int nums1_[] = {-1, 2, 1, -4};
-    std::vector<int> nums1 = vector<int>(nums1_, nums1_+4);
+    bool pass = true;
+    ListNode *l1 = new ListNode(1);
+    l1->next = new ListNode(4);
+    l1->next->next = new ListNode(5);
+
+    ListNode *l2 = new ListNode(1);
+    l2->next = new ListNode(3);
+    l2->next->next = new ListNode(4);
+
+    ListNode *l3 = new ListNode(2);
+    l3->next = new ListNode(6);
+
+    vector<ListNode*> lists;
+    lists.push_back(l1);
+    lists.push_back(l2);
+    lists.push_back(l3);
+
     Solution s;
-    int resu1 = s.threeSumClosest(nums1, 1);
-    if (resu1 == 2) {
+    ListNode* resu1 = s.mergeKLists(lists);
+    int resu__[] = {1,1,2,3,4,4,5,6};
+    vector<int> resu_ = vector<int>(resu__, resu__+8);
+    for (int i = 0; i < resu_.size(); ++i) {
+        if (resu_[i] != resu1->val) {
+            pass = false;
+        }
+        resu1 = resu1->next;
+    }
+    // while (resu1->next != NULL)
+    // {
+    //     std::cout << resu1->val << std::endl;
+    //     resu1 = resu1->next;
+    // }
+    
+    if (pass) {
         std::cout << "pass" << std::endl;
     } else {
         std::cout << "not pass!" << std::endl;
