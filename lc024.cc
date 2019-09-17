e1c4c85d2d3fa959456f4b4c08af72586aec68bb
ChallenAi <jianglun465@sina.com>
2019-09-17 14:28:09 +0800

    AC 24

--- lc024.cc
+++ lc024.cc
@@ -11,6 +11,20 @@ struct ListNode {
 class Solution {
 public:
     ListNode* swapPairs(ListNode* head) {
-        
+        ListNode *resu = new ListNode(0);
+        resu->next = head;
+        ListNode *ptr = resu;
+        while (ptr != NULL && ptr->next != NULL && ptr->next->next != NULL) {
+            swapNextTwo(ptr);
+            ptr = ptr->next->next;
+        }
+        return resu->next;
+    }
+
+    void swapNextTwo(ListNode* ptr) {
+        ListNode *temp = ptr->next;
+        ptr->next = ptr->next->next;
+        temp->next = ptr->next->next;
+        ptr->next->next = temp;
     }
 };
--- lc024_test.cc
+++ lc024_test.cc
@@ -1,14 +1,37 @@
 #include <iostream>
 #include <vector>
-#include "lc016.cc"
+#include "lc024.cc"
 
 int main(int argc, char const *argv[])
 {
-    int nums1_[] = {-1, 2, 1, -4};
-    std::vector<int> nums1 = vector<int>(nums1_, nums1_+4);
+    bool pass = true;
+    ListNode *head = new ListNode(1);
+    head->next = new ListNode(2);
+    head->next->next = new ListNode(3);
+    head->next->next->next = new ListNode(4);
+    head->next->next->next->next = new ListNode(5);
+    head->next->next->next->next->next = new ListNode(6);
+    head->next->next->next->next->next->next = new ListNode(7);
+
     Solution s;
-    int resu1 = s.threeSumClosest(nums1, 1);
-    if (resu1 == 2) {
+    ListNode *resu1 = s.swapPairs(head);
+    int resu_[] = {2, 1, 4, 3, 6, 5, 7};
+    vector<int> resu = vector<int>(resu_, resu_+7);
+    ListNode *ptr = resu1;
+    for (int i = 0; i < resu.size(); ++i) {
+        if (resu[i] != ptr->val) {
+            pass = false;
+        }
+        ptr = ptr->next;
+    }
+
+    // while (ptr != NULL)
+    // {
+    //     std::cout << ptr->val << std::endl;
+    //     ptr = ptr->next;
+    // }
+    
+    if (pass) {
         std::cout << "pass" << std::endl;
     } else {
         std::cout << "not pass!" << std::endl;
