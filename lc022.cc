44ecf0e2110a94f8cbb4a73d83bd43ba803b23b2
ChallenAi <jianglun465@sina.com>
2019-09-17 11:45:16 +0800

    22 notpass

--- lc022.cc
+++ lc022.cc
@@ -1,11 +1,27 @@
 #include <vector>
 #include <string>
+#include <unordered_map>
 
 using namespace std;
 
 class Solution {
 public:
     vector<string> generateParenthesis(int n) {
-        
+        vector<string> resu;
+
+        unordered_map<char, int> cnts;
+        int cnt = 3;
+
+        if (n == 0) {
+            return resu;
+        }
+        for (int i = 0; i < n*2; ++i) {
+            string resu_str = "";
+            for (int idx = 0; idx < 2; ++idx) {
+            }
+        }
+        return resu;   
     }
+
+    vector<string> handleNext(vector<string> before_str, int cnt)
 };
--- lc022_test.cc
+++ lc022_test.cc
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <vector>
 #include <string>
+#include <unordered_map>
 #include "lc022.cc"
 
 int main(int argc, char const *argv[])
@@ -11,9 +12,24 @@ int main(int argc, char const *argv[])
     resu_.push_back("(())()");
     resu_.push_back("()(())");
     resu_.push_back("()()()");
+
+    unordered_map<string, bool> cmps;
+    bool pass = true;
     Solution s;
     vector<string> resu1 = s.generateParenthesis(3);
-    if (false) {
+    if (resu1.size() != resu_.size()) {
+        pass = false;
+    } else {
+        for (int i = 0; i < resu1.size(); i++) {
+            cmps[resu1[i]] = true;
+        }
+        for (int i = 0; i < resu_.size(); ++i) {
+            if (cmps.find(resu_[i]) != cmps.end()) {
+                pass = false;
+            }
+        }
+    }
+    if (pass) {
         std::cout << "pass" << std::endl;
     } else {
         std::cout << "not pass!" << std::endl;
