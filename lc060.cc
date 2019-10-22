ba6f94549ded668e1e4ea86be6e2f08a1d23fc3c
ChallenAi <jianglun465@sina.com>
2019-10-22 19:44:49 +0800

    AC060

--- lc060.cc
+++ lc060.cc
@@ -5,8 +5,50 @@
 using namespace std;
 
 class Solution {
+private:
+    string resu;
+    int k;
+    vector<int> current;
+    int caculateMulti(int n) {
+        int sum = 1;
+        for (int i = 1; i <= n; ++i) {
+            sum *= i;
+        }
+        return sum;
+    }
+
+    void combineString() {
+        if (current.size() == 0) {
+            return;
+        }
+        for (int i = 0; i < current.size(); ++i) {
+            if (k <= caculateMulti(current.size()-1)) {
+                resu += to_string(current[i]);
+                current.erase(current.begin()+i);
+                combineString();
+            } else {
+                k -= caculateMulti(current.size()-1);
+            }
+        }
+    }
 public:
     string getPermutation(int n, int k) {
-        
+        if (n < 1) {
+            return "";
+        }
+
+        this->k = k % caculateMulti(n);
+
+        for (int i = 1; i <= n; ++i) {
+            current.push_back(i);
+        }
+
+        resu = "";
+
+        combineString();
+
+        // std::cout << resu << std::endl;
+
+        return resu;
     }
 };
--- lc060_test.cc
+++ lc060_test.cc
@@ -7,8 +7,10 @@ using namespace std;
 int main(int argc, char const *argv[])
 {
     Solution s;
+    string resu1 = s.getPermutation(3, 3);
+    string resu2 = s.getPermutation(4, 9);
 
-    if (true) {
+    if (resu1 == "213" && resu2 == "2314") {
         std::cout << "pass" << std::endl;
     } else {
         std::cout << "not pass!" << std::endl;
