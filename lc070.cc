687989344d2eec3bb45d60617c7b9a537dd1bbb3
ChallenAi <jianglun465@sina.com>
2019-10-27 10:44:28 +0800

    AC070

--- lc070.cc
+++ lc070.cc
@@ -4,8 +4,31 @@
 using namespace std;
 
 class Solution {
+private:
+    int counter, max_level;
+    void handleNextStair(int level) {
+        if (level == max_level) {
+            counter++;
+            return;
+        }
+
+        if (level +1 < max_level) {
+            handleNextStair(level+2);
+        }
+
+        if (level < max_level) {
+            handleNextStair(level+1);
+        }
+
+        return;
+    }
 public:
     int climbStairs(int n) {
+        counter = 0;
+        max_level = n;
+
+        handleNextStair(0);
         
+        return counter;
     }
 };
