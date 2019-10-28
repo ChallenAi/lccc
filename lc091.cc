f141e97336fc46cdaf97418842bbf55ea2bdc720
ChallenAi <jianglun465@sina.com>
2019-10-28 16:01:14 +0800

    AC 064

--- lc091.cc
+++ lc091.cc
@@ -5,6 +5,18 @@ using namespace std;
 class Solution {
 public:
     int numDecodings(string s) {
-        
+        int counter = 1;
+        char before = s.at(0);
+        for (int i = 1; i < s.size(); ++i) {
+            if (before == '1') {
+                counter++;
+                // turn char to int ~     =>    x - '0'
+            } else if (before == '2' && (int)(s.at(i)-'0')) {
+                counter++;
+            }
+            before = s.at(i);
+        }
+
+        return counter;
     }
 };
