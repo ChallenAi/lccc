3e3977562febf1eb0ff5f0112090db017da02650
ChallenAi <jianglun465@sina.com>
2019-11-17 22:27:02 +0800

    AC 103

--- lc103.cc
+++ lc103.cc
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <vector>
+#include <list>
 
 using namespace std;
 
@@ -11,8 +12,57 @@ struct TreeNode {
 };
 
 class Solution {
+private:
+    int layer_cnt;
+    vector<vector<int> > resu;
+    list<TreeNode*> curr;
+    void recurseLeftOrder() {
+        if (curr.empty()) {
+            return;
+        }
+        layer_cnt = curr.size();
+        vector<int> temp;
+        while (layer_cnt--) {
+            TreeNode* ptr = curr.front();
+            curr.pop_front();
+            temp.push_back(ptr->val);
+            if (ptr->left != NULL) {
+                curr.push_back(ptr->left);
+            }
+            if (ptr->right != NULL) {
+                curr.push_back(ptr->right);
+            }
+        }
+        resu.push_back(temp);
+        recurseRightOrder();
+    }
+
+    void recurseRightOrder() {
+        if (curr.empty()) {
+            return;
+        }
+        layer_cnt = curr.size();
+        vector<int> temp;
+        while (layer_cnt--) {
+            TreeNode* ptr = curr.back();
+            curr.pop_back();
+            temp.push_back(ptr->val);
+            if (ptr->right != NULL) {
+                curr.push_front(ptr->right);
+            }
+            if (ptr->left != NULL) {
+                curr.push_front(ptr->left);
+            }
+        }
+        resu.push_back(temp);
+        recurseLeftOrder();
+    }
+    
 public:
     vector<vector<int>> zigzagLevelOrder(TreeNode* root) {
-        
+        curr.push_back(root);
+        layer_cnt = 1;
+        recurseLeftOrder();
+        return resu;
     }
 };
