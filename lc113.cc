624024ebfb8670b0a80a528ed98680b63e706963
ChallenAi <jianglun465@sina.com>
2019-11-23 15:57:36 +0800

    AC 113

--- lc113.cc
+++ lc113.cc
@@ -1,4 +1,6 @@
 #include <iostream>
+#include <vector>
+#include <stack>
 
 using namespace std;
 
@@ -10,8 +12,29 @@ struct TreeNode {
 };
 
 class Solution {
+private:
+    vector<vector<int> > resu;
+    vector<int> curr;
+    int current_sum, sum;
+    void recurse(TreeNode* ptr) {
+        if (ptr == NULL) {
+            return;
+        }
+        current_sum += ptr->val;
+        curr.push_back(ptr->val);
+        if (ptr->left == NULL && ptr->right == NULL && current_sum == sum) {
+            resu.push_back(curr);
+        }
+        recurse(ptr->left);
+        recurse(ptr->right);
+        curr.pop_back();
+        current_sum -= ptr->val;
+    }
 public:
     vector<vector<int>> pathSum(TreeNode* root, int sum) {
-        
+        this->sum = sum;
+        current_sum = 0;
+        recurse(root);
+        return resu;
     }
 };
--- lc113_test.cc
+++ lc113_test.cc
@@ -1,7 +1,26 @@
 #include <iostream>
+#include <vector>
 #include "lc113.cc"
 
 int main(int argc, char const *argv[])
 {
+    TreeNode* root = new TreeNode(5);
+    root->left = new TreeNode(4);
+    root->right = new TreeNode(8);
+    root->left->left = new TreeNode(11);
+    root->right->left = new TreeNode(13);
+    root->right->right = new TreeNode(4);
+    root->left->left->left = new TreeNode(7);
+    root->left->left->right = new TreeNode(2);
+    root->right->right->left = new TreeNode(5);
+    root->right->right->right = new TreeNode(1);
+    
+    Solution s;
+    vector<vector<int>> resu = s.pathSum(root, 22);
 
+    if (resu.size() == 2) {
+        std::cout << "pass" << std::endl;
+    } else {
+        std::cout << "not pass!" << std::endl;
+    }
 }
