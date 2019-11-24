74988973711f91ad268580094089476fb636ec7e
ChallenAi <jianglun465@sina.com>
2019-11-24 21:41:04 +0800

    AC 117 j

--- java/lc117/src/app/Solution.java
+++ java/lc117/src/app/Solution.java
@@ -1,7 +1,42 @@
 package app;
 
+import java.util.LinkedList;
+import java.util.Queue;
+
 class Solution {
+    private Queue<Node> q;
+    private int layer_cnt;
+    private Node ptr;
+    public Solution() {
+        q = new LinkedList<Node>();
+    }
+    private void recurse() {
+        if (q.isEmpty()) {
+            return;
+        }
+        layer_cnt = q.size();
+        while (layer_cnt > 0) {
+            ptr = q.poll();
+            if (layer_cnt > 1) {
+                ptr.next = q.peek();
+            }
+            if (ptr.left != null) {
+                q.add(ptr.left);
+            }
+            if (ptr.right != null) {
+                q.add(ptr.right);
+            }
+            layer_cnt--;
+        }
+        recurse();
+        return;
+    }
     public Node connect(Node root) {
+        if (root == null) {
+            return null;
+        }
+        q.add(root);
+        recurse();
         return null;
     }
 }
--- java/lc117/src/app/Tester.java
+++ java/lc117/src/app/Tester.java
@@ -1,10 +1,36 @@
 package app;
 
+import java.util.Queue;
+import java.util.LinkedList;
+
 /**
  * Tester
  */
 public class Tester {
     public static void main(String[] args) {
-        
+        Node root = new Node(1, null, null, null);
+        root.left = new Node(2, null, null, null);
+        root.right = new Node(3, null, null, null);
+        root.left.left = new Node(4, null, null, null);
+        root.left.right = new Node(5, null, null, null);
+        root.right.right = new Node(7, null, null, null);
+
+        Solution s = new Solution();
+        s.connect(root);
+
+        Queue<Node> q = new LinkedList<>();
+        Node ptr = root;
+        while (ptr != null) {
+            q.add(ptr);
+            ptr = ptr.left;
+        }
+        while (!q.isEmpty()) {
+            ptr = q.poll();
+            while (ptr != null) {
+                System.out.println(ptr.val);
+                ptr = ptr.next;
+            }
+        }
+        System.out.println("pass");
     }
 }
