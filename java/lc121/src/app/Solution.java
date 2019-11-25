e42f65150befa6d74f8fa118c602a4c476c1740b
ChallenAi <jianglun465@sina.com>
2019-11-25 14:54:35 +0800

    AC 121

--- java/lc121/src/app/Solution.java
+++ java/lc121/src/app/Solution.java
@@ -1,5 +1,51 @@
 package app;
 
 class Solution {
-
+    private int[] dates = {0, 0};
+    private int[] prices;
+    private boolean movePtr() {
+        int left = -1;
+        int right = -1;
+        for (int i = dates[0]+1; i < dates[1]; i++) {
+            if (prices[i] < prices[dates[0]]) {
+                left = i;
+                break;
+            }
+        }
+        for (int i = dates[1]-1; i > dates[0]; i--) {
+            if (prices[i] > prices[dates[1]]) {
+                right = i;
+                break;
+            }
+        }
+        if (left > 0 && right > 0) {
+            if ((prices[dates[0]] - prices[left]) > (prices[right] - prices[dates[1]])) {
+                dates[0] = left;
+            } else {
+                dates[1] = right;
+            }
+            return false;
+        } else if (left > 0) {
+            dates[0] = left;
+            return false;
+        } else if (right > 0) {
+            dates[1] = right;
+            return false;
+        }
+        return true;
+    }
+    public int maxProfit(int[] prices) {
+        this.prices = prices;
+        dates[1] = prices.length -1;
+        while (dates[0] < dates[1]) {
+            if (movePtr()) {
+                break;
+            }
+        }
+        int resu = prices[dates[1]] - prices[dates[0]];
+        if (resu < 0) {
+            return 0;
+        }
+        return resu;
+    }
 }
--- java/lc121/src/app/Tester.java
+++ java/lc121/src/app/Tester.java
@@ -5,10 +5,20 @@ package app;
  */
 public class Tester {
     public static void main(String[] args) {
-        // List<List<Integer>> resuTarget = new ArrayList<>();
+
+        int[] list = {7,1,5,3,6,4};
+        int[] list2 = {7,6,4,3,1};
 
         Solution s = new Solution();
+        int resu = s.maxProfit(list);
 
-        System.out.println("pass");
+        Solution s2 = new Solution();
+        int resu2 = s2.maxProfit(list2);
+
+        if (resu == 5 && resu2 == 0) {
+            System.out.println("pass");
+        } else {
+            System.out.println("not pass");
+        }
     }
 }
