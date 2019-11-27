787119ced673cb9e81b710941f179f3fdefc332c
ChallenAi <jianglun465@sina.com>
2019-11-27 13:32:08 +0800

    AC 120

--- java/lc120/src/app/Solution.java
+++ java/lc120/src/app/Solution.java
@@ -1,5 +1,30 @@
 package app;
 
-class Solution {
+import java.util.List;
 
+class Solution {
+    private List<List<Integer>> triangle;
+    private int minimum, current_sum;
+    private void recurse(int layer, int idx) {
+        current_sum += triangle.get(layer).get(idx);
+        if (layer == triangle.size()-1) {
+            if (current_sum < minimum) {
+                minimum = current_sum;
+            }
+            current_sum -= triangle.get(layer).get(idx);
+            return;
+        }
+        recurse(layer+1, idx);
+        recurse(layer+1, idx+1);
+        current_sum -= triangle.get(layer).get(idx);
+    }
+    public int minimumTotal(List<List<Integer>> triangle) {
+        this.triangle = triangle;
+        minimum = 0;
+        for (List<Integer> el:triangle) {
+            minimum += el.get(0);
+        }
+        recurse(0, 0);
+        return minimum;
+    }
 }
--- java/lc120/src/app/Tester.java
+++ java/lc120/src/app/Tester.java
@@ -1,14 +1,26 @@
 package app;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 /**
  * Tester
  */
 public class Tester {
     public static void main(String[] args) {
-        // List<List<Integer>> resuTarget = new ArrayList<>();
+        List<List<Integer>> triangle = new ArrayList<>();
+        triangle.add(new ArrayList<>(Arrays.asList(2)));
+        triangle.add(new ArrayList<>(Arrays.asList(3, 4)));
+        triangle.add(new ArrayList<>(Arrays.asList(6, 5, 7)));
+        triangle.add(new ArrayList<>(Arrays.asList(4, 1, 8, 3)));
 
         Solution s = new Solution();
-
-        System.out.println("pass");
+        int resu = s.minimumTotal(triangle);
+        if (resu == 11) {
+            System.out.println("pass");
+        } else {
+            System.out.println("not pass");
+        }
     }
 }
