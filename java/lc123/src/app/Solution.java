24ca41c46d51fa708981f3e9a3d00bb55308d2b4
ChallenAi <jianglun465@sina.com>
2019-11-27 14:55:05 +0800

    AC 123

--- java/lc123/src/app/Solution.java
+++ java/lc123/src/app/Solution.java
@@ -1,5 +1,25 @@
 package app;
 
-class Solution {
+import java.lang.Math;
 
+class Solution {
+    private int[] prices;
+    private int dp(int n, boolean isHold, int limit) {
+        if (n == 0) {
+            return isHold ? 0 - prices[0] : 0;
+        }
+        if (limit == 0) {
+            return 0;
+        }
+        if (isHold) {
+            return Math.max(dp(n-1, true, limit), dp(n-1, false, limit-1) - prices[n]);
+        } else {
+            return Math.max(dp(n-1, false, limit), dp(n-1, true, limit) + prices[n]);
+        }
+        // return isHold ? Math.max(dp(n-1, true, limit), dp(n-1, false, limit-1) - prices[n]) : Math.max(dp(n-1, false, limit), dp(n-1, true, limit) + prices[n]);
+    }
+    public int maxProfit(int[] prices) {
+        this.prices = prices;
+        return dp(prices.length-1, false, 2);
+    }
 }
