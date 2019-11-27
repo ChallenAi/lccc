a8f3a3706e37866ad2e9a49f20f14ed7034e2256
ChallenAi <jianglun465@sina.com>
2019-11-27 22:53:45 +0800

    AC 127

--- java/lc127/src/app/Solution.java
+++ java/lc127/src/app/Solution.java
@@ -1,5 +1,47 @@
 package app;
 
-class Solution {
+import java.util.ArrayList;
+import java.util.List;
 
+class Solution {
+    private List<String> wordList, currentList;
+    private int currentLen;
+    private String endWord;
+    private boolean transform(String a, String b) {
+        int counter = 0;
+        for (int i = 0; i < a.length(); i++) {
+            if (a.charAt(i) != b.charAt(i)) {
+                counter++;
+                if (counter > 1) {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+    private void recurse(String target) {
+        if (target == endWord) {
+            if (currentLen > currentList.size()) {
+                currentLen = currentList.size();
+            }
+            return;
+        }
+        for (int i = 0; i < wordList.size(); i++) {
+            String temp = wordList.get(i);
+            if (!currentList.contains(temp) && transform(temp, target)) {
+                currentList.add(temp);
+                recurse(temp);
+                currentList.remove(temp);
+            }
+        }
+    }
+    public int ladderLength(String beginWord, String endWord, List<String> wordList) {
+        this.wordList = wordList;
+        this.endWord = endWord;
+        currentList = new ArrayList<String>();
+        currentList.add(beginWord);
+        currentLen = wordList.size()+2;
+        recurse(beginWord);
+        return currentLen > wordList.size()+1 ? 0 : currentLen;
+    }
 }
--- java/lc127/src/app/Tester.java
+++ java/lc127/src/app/Tester.java
@@ -1,14 +1,25 @@
 package app;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 /**
  * Tester
  */
 public class Tester {
     public static void main(String[] args) {
-        // List<List<Integer>> resuTarget = new ArrayList<>();
+        List<String> wordList = new ArrayList<>(Arrays.asList("hot","dot","dog","lot","log","cog"));
+        String beginWord = "hit";
+        String endWord = "cog";
 
         Solution s = new Solution();
+        int resu = s.ladderLength(beginWord, endWord, wordList);
 
-        System.out.println("pass");
+        if (resu == 5) {
+            System.out.println("pass");
+        } else {
+            System.out.println("not pass");
+        }
     }
 }
