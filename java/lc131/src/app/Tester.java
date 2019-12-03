31d37a5432c17a1ac301d365aa37b7a3c6c6793f
ChallenAi <jianglun465@sina.com>
2019-12-03 17:21:50 +0800

    add 131 - 140 framework

---
+++ java/lc131/src/app/Tester.java
@@ -0,0 +1,13 @@
+package app;
+
+/**
+ * Tester
+ */
+public class Tester {
+    public static void main(String[] args) {
+
+        Solution s = new Solution();
+
+        System.out.println("pass");
+    }
+}
