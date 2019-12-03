c9668ca3f8e3ac15e6416d7fbc9f1cba7c727af7
ChallenAi <jianglun465@sina.com>
2019-12-03 16:38:32 +0800

    AC 129

--- java/lc129/src/app/Solution.java
+++ java/lc129/src/app/Solution.java
@@ -1,5 +1,28 @@
 package app;
 
-class Solution {
+import java.util.ArrayList;
 
+class Solution {
+    int resu;
+    private ArrayList<Integer> current;
+    private void recurse(TreeNode t) {
+        if (t == null) {
+            return;
+        }
+        current.add(t.val);
+        if (t.left == null && t.right == null) {
+            for (int i = current.size()-1; i >= 0; i--) {
+                resu += current.get(i)*Math.pow(10, current.size()-1-i);
+            }
+        }
+        recurse(t.left);
+        recurse(t.right);
+        current.remove(current.size() -1);
+    }
+    public int sumNumbers(TreeNode root) {
+        current = new ArrayList<>();
+        resu = 0;
+        recurse(root);
+        return resu;
+    }
 }
--- java/lc129/src/app/Tester.java
+++ java/lc129/src/app/Tester.java
@@ -5,10 +5,25 @@ package app;
  */
 public class Tester {
     public static void main(String[] args) {
-        // List<List<Integer>> resuTarget = new ArrayList<>();
+
+        TreeNode root1 = new TreeNode(1);
+        root1.left = new TreeNode(2);
+        root1.right = new TreeNode(3);
+        
+        TreeNode root2 = new TreeNode(4);
+        root2.left = new TreeNode(9);
+        root2.right = new TreeNode(0);
+        root2.left.left = new TreeNode(5);
+        root2.left.right = new TreeNode(1);
 
         Solution s = new Solution();
+        int resu1 = s.sumNumbers(root1);
+        int resu2 = s.sumNumbers(root2);
 
-        System.out.println("pass");
+        if (resu1 == 25 && resu2 == 1026) {
+            System.out.println("pass");
+        } else {
+            System.out.println("not pass");
+        }
     }
 }
