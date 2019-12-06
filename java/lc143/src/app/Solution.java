57b9f3609857e0a68b9dfe7210303fbee9a9cb26
ChallenAi <jianglun465@sina.com>
2019-12-06 13:38:33 +0800

    AC 143

--- java/lc143/src/app/Solution.java
+++ java/lc143/src/app/Solution.java
@@ -1,5 +1,27 @@
 package app;
 
-class Solution {
+import java.util.LinkedList;
 
+class Solution {
+    private LinkedList<ListNode> ll = new LinkedList<>();
+    public void reorderList(ListNode head) {
+        ListNode ptr = head;
+        while (ptr != null) {
+            ll.add(ptr);
+            ptr = ptr.next;
+        }
+        head = new ListNode(0);
+        ptr = head;
+        while (ll.size() >= 2) {
+            ptr.next = ll.removeFirst();
+            ptr.next.next = ll.removeLast();
+            ptr = ptr.next.next;
+        }
+        if (!ll.isEmpty()) {
+            ptr.next = ll.removeLast();
+            ptr = ptr.next;
+        }
+        ptr.next = null;
+        head = head.next;
+    }
 }
--- java/lc143/src/app/Tester.java
+++ java/lc143/src/app/Tester.java
@@ -5,8 +5,22 @@ package app;
  */
 public class Tester {
     public static void main(String[] args) {
-
+        ListNode ll = new ListNode(1);
+        ll.next = new ListNode(2);
+        ll.next.next = new ListNode(3);
+        ll.next.next.next = new ListNode(4);
+        ll.next.next.next.next = new ListNode(5);
+        ll.next.next.next.next.next = new ListNode(6);
+        ll.next.next.next.next.next.next = new ListNode(7);
+        
         Solution s = new Solution();
+        s.reorderList(ll);
+        
+        ListNode ptr = ll;
+        while (ptr != null) {
+            System.out.print(ptr.val + " ");
+            ptr = ptr.next;
+        }
 
         System.out.println("pass");
     }
