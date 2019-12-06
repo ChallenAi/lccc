758dd1d39011f7dee930359f63f363a68fecf6dc
ChallenAi <jianglun465@sina.com>
2019-12-06 23:11:01 +0800

    AC 146

--- java/lc146/src/app/Solution.java
+++ java/lc146/src/app/Solution.java
@@ -1,5 +1,72 @@
 package app;
 
-class Solution {
+import java.util.HashMap;
 
+class LRUCache {
+    private class RingNode {
+        public int key;
+        public int value;
+        public RingNode next;
+        public RingNode before;
+        public RingNode(int _key, int _value, RingNode _next, RingNode _before) {
+            key = _key;
+            value = _value;
+            next = _next;
+            before = _before;
+        }
+    }
+    private RingNode head;
+    private HashMap<Integer, RingNode> mapper;
+    final private int UNUSABLE_INT = -99999;
+
+    public LRUCache(int capacity) {
+        if (capacity == 0) {
+            throw new Error("bad capacity");
+        }
+
+        head = new RingNode(UNUSABLE_INT, UNUSABLE_INT, head, head);
+        capacity--;
+        RingNode ptr = head;
+        while (capacity > 0) {
+            ptr.next = new RingNode(UNUSABLE_INT, UNUSABLE_INT, head, ptr);
+            head.before = ptr.next;
+            ptr = ptr.next;
+            capacity--;
+        }
+
+        mapper = new HashMap<>();
+    }
+
+    private void empower(RingNode n) {
+        n.before.next = n.next;
+        n.next.before = n.before;
+
+        n.before = head.before;
+        head.before.next = n;
+        n.next = head;
+        head.before = n;
+        head = n;
+    }
+    
+    public int get(int key) {
+        if (!mapper.containsKey(key)) {
+            return -1;
+        }
+        empower(mapper.get(key));
+        return mapper.get(key).value;
+    }
+    
+    public void put(int key, int value) {
+        if (!mapper.containsKey(key)) {
+            mapper.remove(head.before.key);
+            mapper.put(key, head.before);
+            head = head.before;
+            head.key = key;
+            head.value = value;
+            return;
+        }
+        mapper.get(key).key = key;
+        mapper.get(key).value = value;
+        empower(mapper.get(key));
+    }
 }
--- java/lc146/src/app/Tester.java
+++ java/lc146/src/app/Tester.java
@@ -6,8 +6,25 @@ package app;
 public class Tester {
     public static void main(String[] args) {
 
-        Solution s = new Solution();
+        boolean resu = true;
 
-        System.out.println("pass");
+        LRUCache cache = new LRUCache(2);
+
+        cache.put(1, 1);
+        cache.put(2, 2);
+        if (cache.get(1) != 1) resu = false;
+        cache.put(3, 3);    // evicts key 2
+        // cache.get(2);       // returns -1 (not found)
+        if (cache.get(2) != -1) resu = false;
+        cache.put(4, 5);    // evicts key 1
+        if (cache.get(1) != -1) resu = false;
+        if (cache.get(3) != 3) resu = false;
+        if (cache.get(4) != 5) resu = false;
+
+        if (resu) {
+            System.out.println("pass");
+        } else {
+            System.out.println("not pass");
+        }
     }
 }
