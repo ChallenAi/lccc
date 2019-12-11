89afc35d62355500ff689bee3c78128bf83b64a2
ChallenAi <jianglun465@sina.com>
2019-12-11 13:35:51 +0800

    AC 147

--- java/lc147/src/app/Solution.java
+++ java/lc147/src/app/Solution.java
@@ -1,5 +1,29 @@
 package app;
 
 class Solution {
+    public ListNode insertionSortList(ListNode head) {
+        ListNode ptr = head.next;
+        ListNode innerPtr;
+        
+        while (ptr != null) {
+            ListNode temp = ptr.next;
+            innerPtr = head;
+            if (ptr.val < innerPtr.val) {
+                ptr.next = head;
+                head = ptr;
+                continue;
+            }
+            while (innerPtr != null) {
+                if (ptr.val > innerPtr.val && (innerPtr.next == null || ptr.val < innerPtr.next.val)) {
+                    ptr.next = innerPtr.next;
+                    innerPtr.next = ptr;
+                    break;
+                }
+                innerPtr = innerPtr.next;
+            }
+            ptr = temp;
+        }
 
+        return head;
+    }
 }
--- java/lc147/src/app/Tester.java
+++ java/lc147/src/app/Tester.java
@@ -5,8 +5,18 @@ package app;
  */
 public class Tester {
     public static void main(String[] args) {
+        ListNode head = new ListNode(-1);
+        head.next = new ListNode(5);
+        head.next.next = new ListNode(3);
+        head.next.next.next = new ListNode(4);
+        head.next.next.next.next = new ListNode(0);
 
         Solution s = new Solution();
+        ListNode resu = s.insertionSortList(head);
+        while (resu != null) {
+            System.out.print(resu.val + " ");
+            resu = resu.next;
+        }
 
         System.out.println("pass");
     }
