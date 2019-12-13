e15ffb368259a92fb3c7d10f5c39d19f75d68158
ChallenAi <jianglun465@sina.com>
2019-12-13 11:13:12 +0800

    AC 152

--- java/lc152/src/app/Solution.java
+++ java/lc152/src/app/Solution.java
@@ -1,5 +1,18 @@
 package app;
 
 class Solution {
-
+    public int maxProduct(int[] nums) {
+        int max = 0;
+        int currentProduction = 1;
+        for (int i = 0; i < nums.length; i++) {
+            currentProduction *= nums[i];
+            if (currentProduction > max) {
+                max = currentProduction;
+            }
+            if (nums[i] == 0) {
+                currentProduction = 1;
+            }
+        }
+        return max;
+    }
 }
--- java/lc152/src/app/Tester.java
+++ java/lc152/src/app/Tester.java
@@ -5,11 +5,16 @@ package app;
  */
 public class Tester {
     public static void main(String[] args) {
+        int[] nums1 = {2,3,-2,4};
+        int[] nums2 = {-2,0,-1};
 
+        Solution s1 = new Solution();
+        int resu1 = s1.maxProduct(nums1);
 
-        Solution s = new Solution();
+        Solution s2 = new Solution();
+        int resu2 = s2.maxProduct(nums2);
 
-        if (true) {
+        if (resu1 == 6 && resu2 == 0) {
             System.out.println("pass");
         } else {
             System.out.println("not pass");
