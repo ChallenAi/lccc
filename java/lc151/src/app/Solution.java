98494017393460fba54f3bb8235ffc983f380b0e
ChallenAi <jianglun465@sina.com>
2019-12-13 10:31:17 +0800

    AC 151

--- java/lc151/src/app/Solution.java
+++ java/lc151/src/app/Solution.java
@@ -1,5 +1,19 @@
 package app;
 
 class Solution {
-
+    public String reverseWords(String s) {
+        String resu = "";
+        s = s.trim();
+        int end = s.length()-1;
+        for (int i = s.length()-1; i >= 0; --i) {
+            if (s.charAt(i) == ' ' && s.charAt(i+1) != ' ') {
+                resu += s.substring(i+1, end+1) + " ";
+            }
+            if (s.charAt(i) != ' ' && (i == s.length()-1 || s.charAt(i+1) == ' ')) {
+                end = i;
+            }
+        }
+        resu += s.substring(0, end+1)+" ";
+        return resu;
+    }
 }
--- java/lc151/src/app/Tester.java
+++ java/lc151/src/app/Tester.java
@@ -5,11 +5,20 @@ package app;
  */
 public class Tester {
     public static void main(String[] args) {
-
+        String s1 = "the sky is blue",
+            s2 = "  hello world!  ",
+            s3 = "a good   example";
 
         Solution s = new Solution();
+        String resu1 = s.reverseWords(s1);
+        String resu2 = s.reverseWords(s2);
+        String resu3 = s.reverseWords(s3);
+        
+        System.out.println(resu1);
+        System.out.println(resu2);
+        System.out.println(resu3);
 
-        if (true) {
+        if (resu1.length() > 0 && resu2.length() > 0 && resu3.length() > 0) {
             System.out.println("pass");
         } else {
             System.out.println("not pass");
