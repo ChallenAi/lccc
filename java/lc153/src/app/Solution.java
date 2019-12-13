9ade89ee4b6547c4427ee0ebac732ec45422d282
ChallenAi <jianglun465@sina.com>
2019-12-13 11:25:55 +0800

    AC 153

--- java/lc153/src/app/Solution.java
+++ java/lc153/src/app/Solution.java
@@ -1,5 +1,19 @@
 package app;
 
 class Solution {
+    public int findMin(int[] nums) {
+        int left = 0;
+        int right = nums.length-1;
+        int mid = (left+right) /2;
+        while (mid != left && mid != right) {
+            if (nums[mid] > nums[nums.length-1]) {
+                left = mid;
+            } else {
+                right = mid;
+            }
+            mid = (left+right) /2;
+        }
 
+        return Math.min(nums[left], nums[right]);
+    }
 }
--- java/lc153/src/app/Tester.java
+++ java/lc153/src/app/Tester.java
@@ -5,11 +5,16 @@ package app;
  */
 public class Tester {
     public static void main(String[] args) {
+        int[] nums1 = {3,4,5,1,2};
+        int[] nums2 = {4,5,6,7,0,1,2};
 
+        Solution s1 = new Solution();
+        int resu = s1.findMin(nums1);
+        
+        Solution s2 = new Solution();
+        int resu2 = s2.findMin(nums2);
 
-        Solution s = new Solution();
-
-        if (true) {
+        if (resu == 1 && resu2 == 0) {
             System.out.println("pass");
         } else {
             System.out.println("not pass");
